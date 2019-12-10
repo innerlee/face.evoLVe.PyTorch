4b2f8c70cf393b66c24fce74152dc7b8d5ffa38e
lizz <lizz@sensetime.com>
2019-12-10 14:38:58 +0800

    tiny
    Signed-off-by: lizz <lizz@sensetime.com>

--- tools/facedetect.py
+++ tools/facedetect.py
@@ -1,13 +1,23 @@
 import argparse
 from PIL import Image
-from evolveface import detect_faces
-from evolveface import show_results
+import numpy as np
+from evolveface import detect_faces, show_results
+from evolveface import get_reference_facial_points, warp_and_crop_face
 
 parser = argparse.ArgumentParser(description='find face')
 parser.add_argument("input", help="input image", type=str)
 parser.add_argument("-o", "--out", help="output image file", default='face.jpg')
+parser.add_argument("--crop_size", help="specify size of aligned faces", default=112, type=int)
 args = parser.parse_args()
+crop_size = args.crop_size
+scale = crop_size / 112.
+reference = get_reference_facial_points(default_square=True) * scale
 
 img = Image.open(args.input)
 bounding_boxes, landmarks = detect_faces(img)
 show_results(img, bounding_boxes, landmarks).save('x.jpg')
+
+for i in range(len(landmarks)):
+    facial5points = [[landmarks[i][j], landmarks[i][j + 5]] for j in range(5)]
+    warped_face = warp_and_crop_face(np.array(img), facial5points, reference, crop_size=(crop_size, crop_size))
+    Image.fromarray(warped_face).save(f'y{i}.jpg')
