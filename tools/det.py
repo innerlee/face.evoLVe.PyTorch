e72190214f959eeaa5ff3775067a67feb5dd7935
lizz <lizz@sensetime.com>
2020-03-18 01:12:27 +0800

    fix
    Signed-off-by: lizz <lizz@sensetime.com>

--- tools/det.py
+++ tools/det.py
@@ -8,15 +8,14 @@ import time
 import glob
 
 parser = argparse.ArgumentParser(description='find face')
-parser.add_argument("-i", "--input", help="input image", type=str, default='play/1.jpg')
-parser.add_argument("-o", "--out", help="output image file", default='x.jpg')
+parser.add_argument("input", help="input folder", type=str)
 parser.add_argument("--crop_size", help="specify size of aligned faces", default=112, type=int)
 args = parser.parse_args()
 crop_size = args.crop_size
 scale = crop_size / 112.
 reference = get_reference_facial_points(default_square=True) * scale
 
-files = sorted(glob.glob('data/anli1/*jpg'))
+files = sorted(glob.glob(args.input + '/*jpg'))
 
 start = time.time()
 for f in files:
